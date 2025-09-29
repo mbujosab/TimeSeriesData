770a157fc2d795e4a2ae13d18082dedb4b190b6e
Marcos Bujosa <mbujosab@ucm.es>
2025-09-29 19:30:44 +0200

    Inclusión de datasets de varios libros

---
+++ W.Wei.TimeSeriesAnalysis/DescargaSeries.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# Descarga de todos los conjuntos de datos del libro
+# "Time Series Analysis" de William W. S. Wei
+
+BASE="https://sites.temple.edu/wwei/files/2020/08"
+
+# Series univariantes
+wget -c $BASE/W1.txt
+wget -c $BASE/W2.txt
+wget -c $BASE/W3.txt
+wget -c $BASE/W4.txt
+wget -c $BASE/W5.txt
+wget -c $BASE/W6.txt
+wget -c $BASE/W7.txt
+wget -c $BASE/W8.txt
+wget -c $BASE/W9.txt
+wget -c $BASE/W10.txt
+wget -c $BASE/W11.txt
+wget -c $BASE/W12-1.txt
+wget -c $BASE/W12-2.txt
+wget -c $BASE/W13-1.txt
+wget -c $BASE/W13-2.txt
+wget -c $BASE/W13-3.txt
+wget -c $BASE/W13-4.txt
+wget -c $BASE/W13-5.txt
+wget -c $BASE/W14.txt
+wget -c $BASE/W15-1.txt
+wget -c $BASE/W15-2.txt
+wget -c $BASE/W16.txt
+wget -c $BASE/Data-sets-Book1-Wei.xls
